a959d1c45620df81b756a8584f3a2148788fecd7
Darshitaa27 <Dpatel6206@conestogac.on.ca>
2025-07-27 23:59:33 -0400

    Create header file for TwoLaneCarGame class with function prototypes and constants

--- car_game/car_game/car_game.h
+++ car_game/car_game/car_game.h
@@ -1,1 +1,39 @@
-#pragma once
+// car_game.h
+#ifndef CAR_GAME_H
+#define CAR_GAME_H
+
+#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <string>
+
+class TwoLaneCarGame {
+public:
+    TwoLaneCarGame(const std::string& user);
+    void run();
+
+private:
+    void clearScreen();
+    void drawBackground();
+    void drawUI();
+    void drawRoadLines();
+    int getLaneX(int lane);
+    void drawPlayerCar();
+    void drawEnemyCars();
+    void drawGameOver();
+    void render();
+    void handleInput();
+    void updateGame();
+    void spawnEnemyCar();
+    void checkCollisions();
+    void saveScore();
+    void showLeaderboard();
+    void restart();
+
+    
+};
+
+
+#endif // CAR_GAME_H
